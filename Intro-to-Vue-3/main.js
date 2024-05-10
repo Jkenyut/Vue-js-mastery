51f755491541adbdd6fc4948343b0fe24b30dcf5
satria nur saputro <satrianursaputro06@gmail.com>
2024-05-10 23:53:06 +0700

    attribute binding

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
-    data: function () {
+    data() {
         return {
-            products: "Socks"
+            product: 'Socks',
+            image:'./assets/images/socks_green.jpg',
         }
     }
 })
